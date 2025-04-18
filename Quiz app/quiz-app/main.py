b8db3a4e501d6804f2859bb7fcf27cb02e48c26c
HammadMustafa07 <hammadmustafa6638@gmail.com>
2025-04-18 14:29:04 +0500

    pushing quiz app

---
+++ Quiz app/quiz-app/main.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import random
+
+st.title("Quiz Application")
+
+questions = [
+    {"question": "What is the capita -l of Pakistan?",
+     "options": ["Karachi", "Lahore", "Islamabad", "Peshawar"],
+     "answer": "Islamabad"
+     },
+    {"question": "who is the Founder of Pakistan?",
+     "options": ["Allama Iqbal", "Quaid e Azam", "xyz", "xyz"],
+     "answer": "Quaid e Azam"
+     },
+    {"question": "who is the Governor of Sindh?",
+     "options": ["Allama Iqbal", "Quaid e Azam", "Kamran Khan Tassoori", "xyz"],
+     "answer": "Kamran Khan Tassoori"
+     },
+]
+
+
